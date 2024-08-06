13d7148b7654d73c6b942000fdc11e7f2b2a6e32
Antonio Galea <antonio.galea@gmail.com>
2024-08-06 16:28:23 +0200

    parser.py: handle BSEC data

--- parser/parser.py
+++ parser/parser.py
@@ -3,11 +3,12 @@
 import collections, heatshrink2, struct
 
 def parse_record(data):
-    Header   = collections.namedtuple('Header', 'bme68x_num bme68x_fp sps30_num senseair_num is_compressed')
-    Sensors  = collections.namedtuple('Sensors', 'vcc vpanel bme68x sps30 senseair')
+    Header   = collections.namedtuple('Header', 'bme68x_num bme68x_fp sps30_num senseair_num bsec_num is_compressed')
+    Sensors  = collections.namedtuple('Sensors', 'vcc vpanel bme68x sps30 bsec senseair')
     Bme68x   = collections.namedtuple('Bme68x', 'temp press hum gas')
     Sps30    = collections.namedtuple('Sps30', 'mc_pm1 mc_pm2_5 mc_pm4 mc_pm10 nc_pm0_5 nc_pm1 nc_pm2_5 nc_pm4 nc_pm10 ps')
     Senseair = collections.namedtuple('Senseair', 'conc_ppm temp_cC')
+    Bsec     = collections.namedtuple('Bsec', 'iaq accuracy')
     Output  = collections.namedtuple('Output', 'header data sensors')
     hdr = data[0]
     data = data[1:]
@@ -15,15 +16,16 @@ def parse_record(data):
     bme68x_fp = (hdr & 0x04 != 0)
     sps30_num = (hdr & 0x08 != 0)
     senseair_num = (hdr & 0x10 != 0)
+    bsec_num = (hdr & 0x20 != 0)
     is_compressed = (hdr & 0x80 != 0)
-    header = Header(bme68x_num, bme68x_fp, sps30_num, senseair_num, is_compressed)
+    header = Header(bme68x_num, bme68x_fp, sps30_num, senseair_num, bsec_num, is_compressed)
     if is_compressed:
         data = heatshrink2.decompress(data, window_sz2=8, lookahead_sz2=4)
     offset = 0
     fmt = "2h"
     vcc, vpanel = struct.unpack_from(fmt, data, offset)
     offset += struct.calcsize(fmt)
-    sensors = Sensors(vcc, vpanel, [], [], [])
+    sensors = Sensors(vcc, vpanel, [], [], [], [])
     if bme68x_num:
         fmt = "h" * bme68x_num
         if bme68x_fp:
@@ -53,6 +55,15 @@ def parse_record(data):
         fmt = "Hh"
         sensors.senseair.append(Senseair(*struct.unpack_from(fmt, data, offset)))
         offset += struct.calcsize(fmt)
+    if bsec_num:
+        fmt = "H" * bme68x_num
+        iaq = struct.unpack_from(fmt, data, offset)
+        offset += struct.calcsize(fmt)
+        fmt = "B" * bme68x_num
+        acc = struct.unpack_from(fmt, data, offset)
+        offset += struct.calcsize(fmt)
+        for i in range(bme68x_num):
+            sensors.bsec.append(Bsec(iaq[i], acc[i]))
     assert offset == len(data), "ERROR: offset={}, len(data)={}".format(offset, len(data))
     output = Output(header, data, sensors)
     return output
@@ -79,6 +90,7 @@ if __name__ == "__main__":
             print("BME68X_FP:         ", output.header.bme68x_fp)
             print("SPS30:             ", output.header.sps30_num)
             print("SENSEAIR:          ", output.header.senseair_num)
+            print("BSEC:              ", output.header.bsec_num)
             print("Compressed payload:", output.header.is_compressed)
             print("Raw data length:   ", len(output.data))
             print("Raw data:", end="")
@@ -107,6 +119,8 @@ if __name__ == "__main__":
                 senseair = output.sensors.senseair[0]
                 print("Concentration: %d ppm" % senseair.conc_ppm);
                 print("Temperature: %4.2f °C" % (senseair.temp_cC / 100.));
+            for i, bsec in enumerate(output.sensors.bsec):
+                print("[BSEC_{}] IAQ = {}, accuracy = {}".format(i, bsec.iaq, bsec.accuracy))
             print("")
 
         except Exception as e:
