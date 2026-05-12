75e0579e2388b83e708900b744bb1e050d0efa34
Antonio Galea <antonio.galea@gmail.com>
2026-05-12 12:10:24 +0200

    prepare.sh: handle parameters for make

--- prepare.sh
+++ prepare.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
 cd $(realpath $(dirname $0))
-config=
-if ! [ -z "$1" ]; then
-  if ! [ -f boards.config ]; then
-    echo "File boards.config is missing."
-    echo "Copy boards.config.sample to boards.config and edit as needed"
-    exit 1
-  fi
-  board=$(egrep "^${1}=" boards.config)
-  if [ -z "$board" ]; then
-    echo "Board $1 not found."
-    exit 1
-  fi
-  config=$(echo $board|cut -f2- -d=)
+
+if ! [ -f boards.config ]; then
+  echo "File boards.config is missing."
+  echo "Copy boards.config.sample to boards.config and edit as needed"
+  exit 1
 fi
-env $config make -j flash
+
+board=$1
+shift
+if [ -z "$board" ]; then
+  echo "Missing board name."
+  echo "Please use one of the boards enumerated in boards.config"
+  exit 1
+fi
+
+config=$(awk -v s="${board}" 'index($0, s) == 1' boards.config)
+if [ -z "$config" ]; then
+  echo "Board '$config' not found in boards.config."
+  exit 1
+fi
+config=$(echo $config|cut -f2- -d=)
+echo "### Board '$board' - Config $config ###"
+echo
+
+env $config make -j$(nproc) $*
